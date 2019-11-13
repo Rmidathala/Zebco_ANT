55023d555f252247723328108b25798ae728fa21
Banerjee <494241@cognizant.com>
2019-11-13 10:30:57 +0530

    fixes

--- Zebco_GIT/businesscomponents/HomePageComponents.java
+++ Zebco_GIT/businesscomponents/HomePageComponents.java
@@ -1206,6 +1206,7 @@ public class HomePageComponents extends ReusableLibrary {
 				Thread.sleep(10000);
 				//commonFunction.clickIfElementPresentJavaScript(getPageElement(HomePageObjects.btnStartAnAdventure),
 						//HomePageObjects.btnStartAnAdventure.getObjectname());	
+				driver.navigate().refresh();
 				commonFunction.clickIfElementPresent(getPageElement(HomePageObjects.btnStartAnAdventure),
 						HomePageObjects.btnStartAnAdventure.getObjectname());
 				Thread.sleep(10000);
